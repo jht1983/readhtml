8a25ac85088475bc62b3416c10fae3f7fac12f3b
jht1983 <jht1983@gmail.com>
2017-06-09 19:10:10 +0800

    Create getjpg.py

--- getjpg.py
+++ getjpg.py
@@ -6,9 +6,9 @@ def gethtml(url):
     return urllib.urlopen(url).read()
     
 def getimg(html):
-    img_re = re.compile(r'src=“(http:.+\.jpg)')
-    imgList = re.findall(img_re,html)
-    imgNo = 1
+    img_re = re.compile(r'src="(http:.+\.jpg)')
+    imgList = re.findall(img_re,html)
+    imgNo = 
     for imgAddr in imgList:
          urllib.urlretrieve(imgAddr,"img%s.jpg" % imgNo)
          imgNo += 1
