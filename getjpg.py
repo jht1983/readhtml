70a4b184b5912446d3881a396ed7325fcca83575
jht1983 <jht1983@gmail.com>
2017-06-09 18:59:57 +0800

    Create getjpg.py

--- getjpg.py
+++ getjpg.py
@@ -6,13 +6,13 @@ def gethtml(url):
     return urllib.urlopen(url).read()
     
 def getimg(html):
-    img_re = re.compile(r'src=(http:.+\.jpg)" width')
-    imgList = findall(img_re,html)
+    img_re = re.compile(r'src=“(http:.+\.jpg)')
+    imgList = re.findall(img_re,html)
     imgNo = 1
     for imgAddr in imgList:
-         urllib.urlretrieve(os.path.join(url,"img%s.jpg" % imgNo)
+         urllib.urlretrieve(imgAddr,"img%s.jpg" % imgNo)
          imgNo += 1
-getimg(gethtml('http://www.baidu.com'))
+getimg(gethtml('http://www.ifeng.com'))
 
 print 'finish!'
 
